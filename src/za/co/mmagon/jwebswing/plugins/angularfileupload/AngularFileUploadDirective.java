1281c1205afc71f38d67aa9cb2e9c6253fdf2481
Marc Magon <ged_marc@hotmail.com>
2018-03-10 14:57:31 +0200

    Min references text

--- src/za/co/mmagon/jwebswing/plugins/angularfileupload/AngularFileUploadDirective.java
+++ src/za/co/mmagon/jwebswing/plugins/angularfileupload/AngularFileUploadDirective.java
@@ -9,7 +9,8 @@ import za.co.mmagon.jwebswing.base.angular.directives.AngularDirectiveBase;
  * @author Marc Magon
  * @since 30 May 2017
  */
-class AngularFileUploadDirective extends AngularDirectiveBase
+public class AngularFileUploadDirective
+		extends AngularDirectiveBase
 {
 
 	private static final long serialVersionUID = 1L;
@@ -25,6 +26,7 @@ class AngularFileUploadDirective extends AngularDirectiveBase
 	@Override
 	public String renderFunction()
 	{
-		return FileTemplates.getFileTemplate(AngularFileUploadDirective.class, "AngularFileUploadDirective", "angularfileuploaddirective").toString();
+		return FileTemplates.getFileTemplate(AngularFileUploadDirective.class, "AngularFileUploadDirective", "angularfileuploaddirective")
+		                    .toString();
 	}
 }
