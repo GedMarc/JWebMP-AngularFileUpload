a0027de5a22d5e1d87735b23efc62b5cbed37208
GedMarc <ged_marc@hotmail.com>
2018-08-18 19:19:03 +0200

    BlueImp separation from Angular Directive

--- src/jre10/java/module-info.java
+++ src/jre10/java/module-info.java
@@ -1,24 +1,9 @@
-import com.jwebmp.core.base.angular.services.IAngularConfiguration;
-import com.jwebmp.core.base.angular.services.IAngularController;
 import com.jwebmp.core.base.angular.services.IAngularDirective;
-import com.jwebmp.core.base.angular.services.IAngularModule;
-import com.jwebmp.core.services.IPageConfigurator;
-import com.jwebmp.guicedservlets.services.IGuiceSiteBinder;
-import com.jwebmp.plugins.angularfileupload.AngularFileUploadBinderGuiceSiteBinder;
-import com.jwebmp.plugins.angularfileupload.AngularFileUploadDataBinderConfigurationBase;
-import com.jwebmp.plugins.angularfileupload.AngularFileUploadPageConfigurator;
-import com.jwebmp.plugins.angularfileupload.angular.AngularBlueImpFileUploadModule;
 import com.jwebmp.plugins.angularfileupload.angular.AngularFileUploadDirective;
-import com.jwebmp.plugins.angularfileupload.angular.BlueImpFileDestroyController;
 
 module com.jwebmp.plugins.angularfileupload {
-	exports com.jwebmp.plugins.angularfileupload;
+
 	exports com.jwebmp.plugins.angularfileupload.angular;
-	exports com.jwebmp.plugins.angularfileupload.intercepters;
-	exports com.jwebmp.plugins.angularfileupload.options;
-	exports com.jwebmp.plugins.angularfileupload.parts;
-	exports com.jwebmp.plugins.angularfileupload.parts.json;
-	exports com.jwebmp.plugins.angularfileupload.servlets;
 
 	requires com.jwebmp.core;
 	requires com.jwebmp.logmaster;
@@ -26,27 +11,16 @@ module com.jwebmp.plugins.angularfileupload {
 	requires java.logging;
 	requires javax.servlet.api;
 	requires com.google.guice;
-	requires commons.fileupload;
+
 	requires com.jwebmp.guicedinjection;
-	requires tika.core;
 
 	requires java.validation;
 	requires com.fasterxml.jackson.annotation;
 	requires com.fasterxml.jackson.databind;
 	requires java.activation;
 
-	provides IGuiceSiteBinder with AngularFileUploadBinderGuiceSiteBinder;
-	provides IPageConfigurator with AngularFileUploadPageConfigurator;
-	provides IAngularModule with AngularBlueImpFileUploadModule;
-	provides IAngularController with BlueImpFileDestroyController;
-	provides IAngularConfiguration with AngularFileUploadDataBinderConfigurationBase;
 	provides IAngularDirective with AngularFileUploadDirective;
 
-	opens com.jwebmp.plugins.angularfileupload to com.fasterxml.jackson.databind, com.jwebmp.core;
-	opens com.jwebmp.plugins.angularfileupload.options to com.fasterxml.jackson.databind, com.jwebmp.core;
-	opens com.jwebmp.plugins.angularfileupload.interfaces to com.fasterxml.jackson.databind, com.jwebmp.core;
 	opens com.jwebmp.plugins.angularfileupload.angular to com.fasterxml.jackson.databind, com.jwebmp.core;
-	opens com.jwebmp.plugins.angularfileupload.parts to com.fasterxml.jackson.databind, com.jwebmp.core;
-	opens com.jwebmp.plugins.angularfileupload.servlets to com.fasterxml.jackson.databind, com.jwebmp.core;
 
 }
