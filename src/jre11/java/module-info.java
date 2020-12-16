ed212f7c8b6988a7498dd2cdaae491e3414b8457
GedMarc <ged_marc@hotmail.com>
2020-12-16 22:46:42 +0200

    Turn on Type-Safety in JWebMP, Hibernate 5.4.26, JAnsi 2.2.1

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -1,3 +1,5 @@
+import com.jwebmp.plugins.angularfileupload.implementations.AngularFileUploadModuleInclusion;
+
 module com.jwebmp.plugins.angularfileupload {
 
 	exports com.jwebmp.plugins.angularfileupload.angular;
@@ -11,7 +13,7 @@ module com.jwebmp.plugins.angularfileupload {
 
 	provides com.jwebmp.core.base.angular.services.IAngularModule with com.jwebmp.plugins.angularfileupload.angular.AngularFileUploadModule, com.jwebmp.plugins.angularfileupload.angular.AngularFilesUploadModule;
 	provides com.jwebmp.core.services.IPageConfigurator with com.jwebmp.plugins.angularfileupload.angular.AngularFileUploadPageConfigurator;
-
+	provides com.guicedee.guicedinjection.interfaces.IGuiceScanModuleInclusions with AngularFileUploadModuleInclusion;
 	provides com.guicedee.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.angularfileupload.implementations.AngularFileUploadExclusionsModule;
 
 	opens com.jwebmp.plugins.angularfileupload.angular to com.fasterxml.jackson.databind, com.jwebmp.core;
--- src/main/java/com/jwebmp/plugins/angularfileupload/angular/AngularFileUploadPageConfigurator.java
+++ src/main/java/com/jwebmp/plugins/angularfileupload/angular/AngularFileUploadPageConfigurator.java
@@ -2,16 +2,38 @@ package com.jwebmp.plugins.angularfileupload.angular;
 
 import com.jwebmp.core.Page;
 import com.jwebmp.core.base.references.JavascriptReference;
+import com.jwebmp.core.plugins.PluginInformation;
+import com.jwebmp.core.plugins.PluginStatus;
 import com.jwebmp.core.services.IPageConfigurator;
 
 import jakarta.validation.constraints.NotNull;
 
+@PluginInformation(pluginName = "Angular File Upload",
+		pluginDescription = "Angular File Model is a directive for angularjs to help you make a model for input file and you can send it to sever for next step.",
+		pluginUniqueName = "angular",
+		pluginVersion = "1.0.4",
+		pluginDependancyUniqueIDs = "jquery",
+		pluginCategories = "jquery, angular, data-binding, ng, google",
+		pluginGitUrl = "https://github.com/GedMarc/JWebMP-AngularJS",
+		pluginSourceUrl = "https://github.com/resultsystems/ng-files-model",
+		pluginWikiUrl = "https://github.com/GedMarc/JWebMP-AngularJS/wiki",
+		pluginOriginalHomepage = "https://github.com/resultsystems/ng-files-model",
+		pluginDownloadUrl = "https://mvnrepository.com/artifact/com.jwebmp.plugins.angular/jwebmp-angular-file-upload",
+		pluginIconImageUrl = "https://angularjs.org/img/AngularJS-large.png",
+		pluginIconUrl = "https://angularjs.org/img/AngularJS-large.png",
+		pluginLastUpdatedDate = "2020/12/14",
+		pluginStatus = PluginStatus.Released,
+		pluginGroupId = "com.jwebmp.plugins.angular",
+		pluginArtifactId = "jwebmp-angular-file-upload",
+		pluginModuleName = "com.jwebmp.plugins.angularfileupload",
+		pluginSubtitle = "Binds the input file type to an angular variable"
+)
 public class AngularFileUploadPageConfigurator
 		implements IPageConfigurator<AngularFileUploadPageConfigurator>
 {
-	private static final JavascriptReference fileUploadReference = new JavascriptReference("AngularFileUploadReference", 1.0,
+	private static final JavascriptReference fileUploadReference = new JavascriptReference("AngularFileUploadReference", 1.04,
 	                                                                                       "bower_components/ng-file-model/ng-file-model.min.js");
-	private static final JavascriptReference fileUploadsReference = new JavascriptReference("AngularFileUploadsReference", 1.0,
+	private static final JavascriptReference fileUploadsReference = new JavascriptReference("AngularFileUploadsReference", 1.04,
 	                                                                                        "bower_components/ng-files-model/ng-files-model.min.js");
 	private static boolean enabled = true;
 
